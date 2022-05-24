f3475ad87b62b731ac87db2fae33b9566ee7575a
AdrianPFMV <103995750+AdrianPFMV@users.noreply.github.com>
2022-05-24 19:02:16 +0200

    Update JuegoBarcosMain.java

--- src/barcosP/JuegoBarcosMain.java
+++ src/barcosP/JuegoBarcosMain.java
@@ -3,12 +3,7 @@ package barcosP;
 public class JuegoBarcosMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		JuegoBarcos hundir = new JuegoBarcos();
-		System.out.println("Hola");
-		hundir.ejecutarJuego();
-
+	
 	}
 
 }
